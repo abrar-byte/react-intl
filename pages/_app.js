bc881d5b588291eb974a8359f112dc8143f54007
abrar-byte <usamahhafidz99@gmail.com>
2022-07-20 17:32:36 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import ar from "../lang/ar.json";
+import en from "../lang/en.json";
+import fr from "../lang/fr.json";
+import nl_NL from "../lang/nl-NL.json";
+
+import "../styles/globals.css";
+
+const messages = {
+  ar,
+  en,
+  fr,
+  "nl-NL": nl_NL,
+};
+
+function getDirection(locale) {
+  if (locale === "ar") {
+    return "rtl";
+  }
+
+  return "ltr";
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  const { locale } = useRouter();
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Component {...pageProps} dir={getDirection(locale)} />
+    </IntlProvider>
+  );
+}
+
+export default MyApp;
